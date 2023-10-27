8d34df5e821dae4b7a4973ca5af738bb4d534c0b
joshua-gibson <joshua.gibson.dev@gmail.com>
2023-10-27 17:58:49 +0100

    feat: background image

--- src/Header/header.tsx
+++ src/Header/header.tsx
@@ -8,7 +8,7 @@ interface Props {
 export const Header: React.FC<Props> = ({  children }) => {
   return (
     <>
-      <h1>{children}</h1>
+      <h1 className='headerClass'>{children}</h1>
     </>
   );
 };
