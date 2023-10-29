4fa431f54f9ee63983c285f2e6b9352ecd853eb3
joshua-gibson <joshua.gibson.dev@gmail.com>
2023-10-29 16:15:12 +0000

    feat: updated structure to take subheadings

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,17 @@
-import {TopShelf} from './components/TopShelf'
-import {BottomShelf} from './components/BottomShelf'
-import {StoryWrapper} from './components/StoryWrapper'
-import "tailwindcss/tailwind.css"
+import { TopShelf } from "./components/TopShelf";
+import { BottomShelf } from "./components/BottomShelf";
+import { StoryWrapper } from "./components/StoryWrapper";
+import "tailwindcss/tailwind.css";
 
 function App() {
-
-
   return (
     <div className="flex flex-col h-screen">
-<TopShelf/>
-<BottomShelf>
-<StoryWrapper stringSelector="ch1"/>
-</BottomShelf>
-</div>
-
-  )
+      <TopShelf />
+      <BottomShelf>
+        <StoryWrapper stringSelector="ch3" />
+      </BottomShelf>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/components/StoryWrapper/storyWrapper.tsx
+++ src/components/StoryWrapper/storyWrapper.tsx
@@ -1,30 +1,52 @@
 import React from "react";
-import "../../assets/fonts/fonts.css"
-import strings from "../../assets/strings.json"
+import "../../assets/fonts/fonts.css";
+import strings from "../../assets/strings.json";
+
+interface Sections {
+  heading?: string;
+  paragraphs: string[];
+}
 
 interface Story {
-    title: string;
-    paragraphs: string[];
-  }
-  
-  interface Stories {
-    [key: string]: Story;
-  }
-  
-  interface Strings {
-    story: Stories;
-  }
-  
+  title: string;
+  sections: Sections[];
+}
+
+interface Stories {
+  [key: string]: Story;
+}
+
+interface Strings {
+  story: Stories;
+}
+
 interface Props {
-    stringSelector: string;
-  }
+  stringSelector: string;
+}
 
 const typedStrings: Strings = strings;
 
-export const StoryWrapper: React.FC<Props> = ({stringSelector}) => {
+export const StoryWrapper: React.FC<Props> = ({ stringSelector }) => {
   return (
-    <div className="text-center  pt-12 pb-12">
-{typedStrings?.story?.[stringSelector]?.title}
-    </div>
+    <>
+      <h1 className="text-center  pb-6">
+        {typedStrings?.story?.[stringSelector]?.title}
+      </h1>
+
+      <div className="text-center">
+        {typedStrings?.story?.[stringSelector]?.sections?.map((y) => {
+          return (
+            <>
+              {y.heading ? (
+                <h2 className="text-3xl pb-2">{y.heading}</h2>
+              ) : null}
+              {y.paragraphs.map((x) => (
+                <p className="pb-2">{x}</p>
+              ))}
+            </>
+          );
+        })}
+      </div>
+    </>
   );
 };
