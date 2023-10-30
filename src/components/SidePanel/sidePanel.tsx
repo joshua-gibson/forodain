c3277ac597469f7b011ba8058f5450b05427d736
joshua-gibson <joshua.gibson.dev@gmail.com>
2023-10-30 21:52:56 +0000

    feat: panel animations

--- src/components/SidePanel/sidePanel.tsx
+++ src/components/SidePanel/sidePanel.tsx
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import "../../assets/fonts/fonts.css";
 
 export const SidePanel: React.FC = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <>
-      <div className="text-center rounded-e-lg bg-neutral-800 border-neutral-600 border-y-2 border-r-2 w-10 h-[43vh] absolute left-0 mt-5">
+      <div
+        className={`text-center rounded-e-lg bg-neutral-800 border-neutral-600 border-y-2 border-r-2 z-10 absolute h-[43vh] left-0 mt-5 transition-all duration-500 ease-in-out ${
+          isOpen ? "w-[500px]" : "w-10"
+        } `}
+      >
         <img
-          className="object-scale-down h-16 fixed left-2 mt-10 rotate-90"
+          onClick={handleClick}
+          className={`object-scale-down h-16 absolute mt-10  transition-all duration-500 ease-in-out z-20 ${
+            isOpen
+              ? "left-[500px] -translate-x-1/2 -rotate-90"
+              : "left-4 rotate-90"
+          }`}
           src="./src/assets/img/arrow-icon.png"
           alt="Broken Spear"
         />
