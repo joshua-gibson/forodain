217175932269be419f865e396651cdafaf6a9354
joshua-gibson <joshua.gibson.dev@gmail.com>
2023-11-02 22:23:56 +0000

    feat: fixed width problem on chrome

--- src/components/TopShelf/topShelf.tsx
+++ src/components/TopShelf/topShelf.tsx
@@ -5,7 +5,7 @@ import "../../assets/fonts/fonts.css";
 export const TopShelf: React.FC = () => {
   return (
     <div className="flex flex-col-reverse  flex-1  ">
-      <div className="w-screen h-4/5 pb-96 ">
+      <div className="w-full h-4/5 pb-96 ">
         <Header>
           <h1 className="text-8xl">Chronicles</h1>
           <h3 className="text-5xl pt-4">of the</h3>
