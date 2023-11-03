1ad027d3efbc7ac7d5491fcaf2bfe2ec5618d258
joshua-gibson <joshua.gibson.dev@gmail.com>
2023-11-03 20:58:09 +0000

    fix: image relative path fix test x3

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,13 @@ import { TopShelf, BottomShelf, StoryPanel } from "./components";
 import "https://flackr.github.io/scroll-timeline/dist/scroll-timeline.js";
 import "tailwindcss/tailwind.css";
 import "./main.css";
+import forestImg from "./src/assets/img/forest.jpg";
 
 function App() {
   const [chapter, setChapter] = useState("ch01");
   return (
     <div className="parallax">
-      <img className="parallax__bg" src="/src/assets/img/forest.jpg" alt="" />
+      <img className="parallax__bg" src={forestImg} alt="" />
       <img className="parallax__dust" src="/src/assets/img/dust3.webp" alt="" />
       <div className="flex flex-col h-screen z-30">
         <TopShelf />
