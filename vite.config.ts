856aba0e6bc2b722fe10e5c4e3a389bb4894c303
joshua-gibson <joshua.gibson.dev@gmail.com>
2023-11-03 20:14:40 +0000

    fix: updated base

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/forodain/',
+  base: 'https://joshua-gibson.github.io/forodain/',
   plugins: [react({include: ['**/*.tsx']})],
 })
