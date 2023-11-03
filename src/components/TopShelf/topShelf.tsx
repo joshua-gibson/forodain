5f5f1799c761aee3b51c417c2fd3e2f99eed605d
joshua-gibson <joshua.gibson.dev@gmail.com>
2023-11-03 14:56:45 +0000

    feat: mobile sizes look fine now

--- src/components/TopShelf/topShelf.tsx
+++ src/components/TopShelf/topShelf.tsx
@@ -5,11 +5,11 @@ import "../../assets/fonts/fonts.css";
 export const TopShelf: React.FC = () => {
   return (
     <div className="flex flex-col-reverse  flex-1  ">
-      <div className="w-full h-4/5 pb-96 ">
+      <div className="w-full h-4/5 pb-56 md:pb-96 ">
         <Header>
-          <h1 className="text-8xl">Chronicles</h1>
-          <h3 className="text-5xl pt-4">of the</h3>
-          <h1 className="text-8xl">Broken Spear</h1>
+          <h1 className="text-5xl md:text-8xl">Chronicles</h1>
+          <h3 className="text-3xl md:text-5xl pt-4">of the</h3>
+          <h1 className="text-5xl md:text-8xl">Broken Spear</h1>
         </Header>
         <TopButtonArea />
       </div>
