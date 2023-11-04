1fdcbf160009c43a8b2f6d09cc0f016a3d69391b
joshua-gibson <joshua.gibson.dev@gmail.com>
2023-11-04 11:16:47 +0000

    feat: responsive top menu

--- src/components/TopButtonArea/topButtonArea.tsx
+++ src/components/TopButtonArea/topButtonArea.tsx
@@ -12,21 +12,22 @@ export const TopButtonArea: React.FC = () => {
     <div className="relative text-center  pt-12 pb-12">
       <button
         onClick={handleButtonClick}
-        className="absolute z-30 left-1/2 -translate-x-1/2 bg-neutral-900 text-white text-3xl drop-shadow-lg font-nightmarePills pt-1 pb-2 px-24 rounded-full"
+        className="absolute z-40 left-1/2 -translate-x-1/2 bg-neutral-900 text-white text-xl md:text-3xl drop-shadow-lg font-nightmarePills pt-1 pb-2 w-48 md:w-80 rounded-full transition-all"
       >
         The Story
       </button>
       {/* extender */}
       <div
-        className={`absolute flex justify-between z-20 left-1/2 -translate-x-1/2 pt-2 px-8 top-12 right-[54rem] bg-neutral-900 text-neutral-600 font-nightmarePills text-2xl rounded-full h-12 transition-all w-2/3 duration-[500ms] ${
-          isOpen ? "md:scale-x-100" : "md:scale-x-0 delay-[300ms]"
+        className={`absolute flex flex-col md:flex-row justify-between z-30 h-60 w-40 md:h-12 md:w-2/3 left-1/2 -translate-x-1/2 py-2 px-8 top-12 right-[54rem] bg-neutral-800 text-neutral-600 font-nightmarePills  text-lg md:text-2xl  rounded-b-xl md:rounded-full max-sm:translate-y-2  transition-all origin-top md:origin-center duration-[500ms] ${
+          isOpen
+            ? "max-sm:scale-y-100 md:scale-x-100"
+            : "max-sm:scale-y-0 md:scale-x-0 delay-[300ms]"
         }`}
       >
-        <p>The Characters</p>
-        <p>The World</p>
-        <div className="w-48"></div>
-        <p>The Denizens</p>
-        <p>The Deities</p>
+        <p className="pt-8 md:pt-0">Characters</p>
+        <p className="pr-0 md:pr-36">World</p>
+        <p className="pl-0 md:pl-36">Denizens</p>
+        <p>Deities</p>
       </div>
     </div>
   );
--- src/components/TopShelf/topShelf.tsx
+++ src/components/TopShelf/topShelf.tsx
@@ -5,7 +5,7 @@ import "../../assets/fonts/fonts.css";
 export const TopShelf: React.FC = () => {
   return (
     <div className="flex flex-col-reverse  flex-1  ">
-      <div className="w-full h-4/5 pb-56 md:pb-96 ">
+      <div className="w-full h-4/5 pb-56 md:pb-96 transition-all">
         <Header>
           <h1 className="text-5xl md:text-8xl">Chronicles</h1>
           <h3 className="text-3xl md:text-5xl pt-4">of the</h3>
